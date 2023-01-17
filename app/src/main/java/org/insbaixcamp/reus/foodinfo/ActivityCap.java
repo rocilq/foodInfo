7b24a66fc7ce2c1e4dd7449c409905f8937eff59
rocilq <roziolq@gmail.com>
2023-01-17 16:02:17 +0100

    camara uwu

---
+++ app/src/main/java/org/insbaixcamp/reus/foodinfo/ActivityCap.java
@@ -0,0 +1,6 @@
+package org.insbaixcamp.reus.foodinfo;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class ActivityCap extends CaptureActivity {
+}
--- app/src/main/java/org/insbaixcamp/reus/foodinfo/MainActivity.java
+++ app/src/main/java/org/insbaixcamp/reus/foodinfo/MainActivity.java
@@ -1,14 +1,64 @@
 package org.insbaixcamp.reus.foodinfo;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.journeyapps.barcodescanner.CaptureActivity;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        camera = findViewById(R.id.button_cam);
+        camera.setOnClickListener(this);
+
+
     }
+
+    @Override
+    public void onClick(View v) {
+        scanCode();
+    }
+
+    private void scanCode() {
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to flash on");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(ActivityCap.class);
+        barLaucher.launch(options);
+
+    }
+
+    ActivityResultLauncher<ScanOptions> barLaucher = registerForActivityResult(new ScanContract(), result->
+    {
+        if(result.getContents() !=null)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Result");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i)
+                {
+                    dialogInterface.dismiss();
+                }
+            }).show();
+        }
+    });
+
 }
